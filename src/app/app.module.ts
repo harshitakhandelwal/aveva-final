b6b9d5239db772563c40eba0b3d2a9d9696b12a6
Harshita Khandelwal <harshita.khandelwal@Aveva.com>
2018-03-22 15:04:35 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
+
 import { NgModule } from '@angular/core';
-
-
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
-
-
+import { BrowserModule } from '@angular/platform-browser';
+import { MatLoginComponent } from './mat-login/mat-login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ReactiveFormsModule} from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatLoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule, MatFormFieldModule, MatInputModule, MatSnackBarModule } from '@angular/material';
+
+
+@NgModule({
+    imports: [
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule
+    ],
+    exports: [
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSnackBarModule
+    ]
+})
+export class MaterialModule {
+
+}
